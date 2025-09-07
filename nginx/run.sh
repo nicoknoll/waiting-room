bd255399675ec5dea03b968b0baedc007f13e005
Nico Knoll <mail@nico.is>
2025-09-07 09:55:30 +0200

    Waiting Room

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Set default value for WAITING_ROOM_ENABLED
+WAITING_ROOM_ENABLED=${WAITING_ROOM_ENABLED:-"0"}
+
+# Replace environment variables in nginx config
+envsubst "\${WAITING_ROOM_ENABLED}" < /etc/nginx/conf.d/default.conf > /tmp/nginx.conf
+mv /tmp/nginx.conf /etc/nginx/conf.d/default.conf
+
+# Start nginx
 nginx -g 'daemon off;'
